ce9e6dcee0539677be7aad7842feebfd1ac28778
ranjit_thegreat <ranjitrocky73@gmail.com>
2025-07-03 20:14:13 +0530

    conditional_rendering_more

--- Cond_render.jsx
+++ Cond_render.jsx
@@ -39,3 +39,34 @@ const Password = ({isValid}) => {
 };
 
 export default Password
+
+/// 2nd example
+
+import Cart from './Cart.jsx'
+
+export default function App() {
+
+
+  return (
+    <>
+    <Cart/>
+    </>
+  );
+}
+
+//
+
+
+const Cart = () => {
+  const items = ["vegetables", "mobile", "clothes", "milk"]
+  return (
+    <div>
+      {items.length && <h1>You have {items.length} in your cart</h1>}
+      {items.map((item) => (
+        <li key = {Math.random()}>{item}</li>
+      ))}
+    </div>
+  )
+}
+
+export default Cart;
