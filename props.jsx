05fb99565f6b8b2404dc6ff35cc26bbc05ff0e9e
ranjit_thegreat <ranjitrocky73@gmail.com>
2025-07-03 19:02:33 +0530

    learning props

--- props.jsx
+++ props.jsx
@@ -10,20 +10,25 @@ export default function App() {
 
   return (
     <>
-      <Props user={user} />
+      <Props user={user} ismarried = "false" />
     </>
   );
 }
 
+
+
 // this are props component
 
-const Props = ({user}) => {
+
+
+const Props = ({ user, ismarried }) => {
   return (
     <div>
       <h1>Name: {user.name}</h1>
       <h1>age: {user.age}</h1>
       <h1>location: {user.location}</h1>
       <h1>email: {user.email}</h1>
+      <h1>isMarried: {ismarried}</h1>
     </div>
   )
 }
